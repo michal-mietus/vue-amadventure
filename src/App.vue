46a2393705845455d5447750d4eba6b88a40fdc6
goodude123 <assasinmcpl@gmail.com>
2019-03-27 18:27:26 +0100

    Main App Vue file.

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Navigation/>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/base/Navigation.vue'
+
+export default {
+  components: {
+    Navigation,
+  },
+
+  methods: {
+    isLoggedIn: function () {
+      return this.$store.getters.isLoggedIn;
+    },
+    logout: function () {
+      this.$store.dispatch('logout').then(() => {
+        this.$router.push('/login');
+      });
+    },
+  },
+};
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
