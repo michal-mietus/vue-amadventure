b6fa31450bcef4048678f4798c2efa8ca6e19103
goodude123 <assasinmcpl@gmail.com>
2019-03-27 20:55:36 +0100

    Register display errors and password confirm validation.

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <h4>Register</h4>
+    <h2>Register</h2>
     <form @submit.prevent="register">
+      <div v-if="errors" class="errors">
+        <span v-for="error in errors">
+          {{ error }}
+        </span>
+      </div>
       <label for="name">Name</label>
       <div>
           <input id="name" type="text" v-model="name" required autofocus>
@@ -32,20 +37,41 @@
       email: "",
       password: "",
       password_confirmation: "",
+      errors: [],
     };
   },
   methods: {
+    checkPasswords: function () {
+      if (this.password == this.password_confirmation) {
+        return true;
+      } return false;
+    },
+
     register: function() {
-      let data = {
-        name: this.name,
-        email: this.email,
-        password: this.password,
+      this.errors = [];
+      if (this.checkPasswords()) {
+        let data = {
+          username: this.name,
+          email: this.email,
+          password: this.password,
+        };
+        this.$store
+          .dispatch("register", data)
+          .then(() => this.$router.push("/"))
+          .catch(err => console.log(err));
+      } else {
+        let error = "Passwords are not the same"
+        this.errors.push(error);
       };
-      this.$store
-        .dispatch("register", data)
-        .then(() => this.$router.push("/"))
-        .catch(err => console.log(err));
     }
   }
   };
 </script>
+
+<style lang="scss" scoped>
+.errors {
+  font-size: 0.9em;
+  color:red;
+  margin: 0 0 15px;
+}
+</style>
