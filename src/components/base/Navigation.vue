8fe8d493bbb43cec75bffd3af07ac025b7ac9a9d
goodude123 <assasinmcpl@gmail.com>
2019-03-27 18:28:20 +0100

    Navigation component.

---
+++ src/components/base/Navigation.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <span v-if="isLoggedIn == true">
+      <span> | <a @click="logout">Logout</a></span>
+    </span>
+    <span v-else>
+      <span> | <router-link to="/login">Login</router-link></span>
+    </span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+  methods: {
+    isLoggedIn: function () {
+      return this.$store.getters.isLoggedIn;
+    },
+    logout: function () {
+      this.$store.dispatch('logout').then(() => {
+        this.$router.push('/login');
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+template {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+  &.router-link-exact-active {
+    color: #42b983;
+  }
+}
+</style>
