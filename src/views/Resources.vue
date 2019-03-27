07d9978128315796122acc7a656ee0eb36bb72e1
goodude123 <assasinmcpl@gmail.com>
2019-03-27 18:29:23 +0100

    Resources view.

---
+++ src/views/Resources.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Resources</h1>
+    <Resources />
+  </div>
+</template>
+
+<script>
+import Resources from '../components/resources/Resources.vue';
+
+export default {
+  components: {
+    Resources,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
