69e0a4cdbcd8dce14821512edc2ccc10552c0416
goodude123 <assasinmcpl@gmail.com>
2019-03-31 00:05:59 +0100

    OwnedHero view.

---
+++ src/views/OwnedHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="owned">
+    <h2>Your Hero</h2>
+    <OwnedHero/>
+  </div>
+</template>
+
+<script>
+import OwnedHero from '../components/hero/OwnedHero.vue';
+
+export default {
+  components: {
+    OwnedHero,
+  },
+};
+</script>
