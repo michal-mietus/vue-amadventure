f3f901fb753525aefa20a457bd594095759cc509
goodude123 <assasinmcpl@gmail.com>
2019-03-31 00:05:51 +0100

    CreateHero view.

---
+++ src/views/CreateHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="create">
+    <h2>Create Hero</h2>
+    <CreateHero/>
+  </div>
+</template>
+
+<script>
+import CreateHero from '../components/hero/CreateHero.vue';
+
+export default {
+  components: {
+    CreateHero,
+  },
+};
+</script>
