d853ce8d96efd3f66c1ab9e741083644ac5f4bcc
goodude123 <assasinmcpl@gmail.com>
2019-04-13 15:11:46 +0200

    Implemented Statistics form upgrade validation, errors.

--- src/components/hero/owned/StatisticsForm.vue
+++ src/components/hero/owned/StatisticsForm.vue
@@ -4,6 +4,9 @@
       <div class="success" v-if="success">
         <span>{{ success }}</span>
       </div>
+      <div class="error" v-if="errors">
+        <span v-for="error in errors ">{{ error }}</span>
+      </div>
       <h4>Points to use: {{ hero.statistic_points }}</h4>
       <!--<form @submit.prevent="updateStatistics">-->
         <div v-for="statistic in statistics">
@@ -26,6 +29,7 @@ export default {
       statistics: null,
       hero: null,
       success: null,
+      errors: [],
     };
   },
   methods: {
@@ -37,7 +41,15 @@ export default {
       let classes = event.srcElement.className.split(' ');
       for (let i=0; i<this.statistics.length; i++){
         if (classes.includes(this.statistics[i].name)) {
-          this.statistics[i].points += 1;
+          if (this.hero.statistic_points <= 0){
+            let error = 'You don\'t have enough statistic points.'
+            if (!this.errors.includes(error)) {
+              this.errors.push(error);
+            };  
+          } else {
+            this.statistics[i].points += 1;
+            this.hero.statistic_points -= 1;
+          };
         };
       };
     },
@@ -45,7 +57,16 @@ export default {
       let classes = event.srcElement.className.split(' ');
       for (let i=0; i<this.statistics.length; i++){
         if (classes.includes(this.statistics[i].name)) {
-          this.statistics[i].points -= 1;
+          let decreasedStatisticPoints = this.statistics[i].points - 1;
+          if (decreasedStatisticPoints < this.statistics[i].pointsCopy){
+            let error = 'You can\'t decrease your statistic points.'
+            if (!this.errors.includes(error)) {
+              this.errors.push(error);
+            };  
+          } else {
+            this.statistics[i].points -= 1;
+            this.hero.statistic_points += 1;
+          };
         };
       };
     },
@@ -66,8 +87,9 @@ export default {
     },
     sendStatisticsToApi: function () {
       let statistics = this.deletePointsCopies();
+      let heroStatisticPoints = this.hero.statistic_points;
       this.$http
-        .post(`${this.$store.state.url}hero/statistic/all/upgrade/`, {statistics})
+        .post(`${this.$store.state.url}hero/statistic/all/upgrade/`, {statistics, heroStatisticPoints})
         .then(response => (this.successfullyUpgraded()))
         .catch(error => (this.errors.push(error)))
     },
@@ -78,6 +100,8 @@ export default {
       // this allows user to decreasing their abilities
     },
     deletePointsCopies: function () {
+      // TODO program is deleting data from this.statistics instead of copy. 
+      // That occurs with error when user send data, he can change it as he want, bcs there isnt pointsCopy
       let statistics = this.statistics;
       for (let i=0; i<statistics.length; i++) {
         delete statistics[i].pointsCopy;
@@ -97,6 +121,10 @@ export default {
 <style lang="scss" scoped> 
 .success {
   color: green;
+  font-weight: bold;
+}
+.error {
+  color: red;
   font-weight: bold;
 }
 </style>
