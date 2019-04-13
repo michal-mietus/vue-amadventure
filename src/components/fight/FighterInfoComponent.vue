cad0f461f24602902757804bcb091655fbd96a32
goodude123 <assasinmcpl@gmail.com>
2019-04-13 15:11:23 +0200

    Component to present all info about fighter: statistics, name etc.

---
+++ src/components/fight/FighterInfoComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="data" class="container">
+    <p v-if="data.object" class="name">
+      {{ data.object.name}}
+    </p>
+    <div v-for="statistic in data.statistics" class="statistic">
+      <p>{{ statistic.name }}: {{ statistic.points}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  // socket keys are socket emits names
+  props: {
+    data: {
+      type: null,
+    },
+  },
+
+  sockets: {
+    
+  },
+  
+  mounted() {
+    
+  },
+  methods: {
+
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-template-columns: 1fr;
+}
+.name {
+  border: 1px solid;
+  padding: 20px;
+  font-weight: bold;
+}
+</style>
