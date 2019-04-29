b632b27a6f60f27b13d2e99fcfb9629f9ddf7d19
goodude123 <assasinmcpl@gmail.com>
2019-04-30 00:17:58 +0200

    Drop item logic.

--- src/views/fight/Fight.vue
+++ src/views/fight/Fight.vue
@@ -11,7 +11,7 @@
       </div>
       <div class="abilities-container border">
         <h3>Abilities</h3>
-        <AbilityComponent v-for="ability in abilities" :ability="ability" class="ability" />
+        <AbilityComponent v-for="ability in abilities" :ability="ability" :key="ability.pk" class="ability" />
       </div>
     </div>
     <div>
@@ -20,18 +20,34 @@
   </div>
   <div v-else-if="winner">
     <p>{{ this.winner.additionalText }} </p>
+    <div v-if="droppedItem">
+      <p>You dropped item!</p>
+      <ItemComponent 
+        :item="droppedItem.item" 
+        :itemStatistics="droppedItem.item_statistics"
+      />
+    </div>
+    <div>
+      <p>Do you want to keep this item?</p>
+      <p>
+        <span @click="keepItem">Yes</span>
+        <span>No</span>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import FighterInfoComponent from '@/components/fight/FighterInfoComponent.vue';
 import AbilityComponent from '@/components/fight/AbilityComponent.vue';
+import ItemComponent from '@/components/artifical/ItemComponent.vue';
 
 
 export default {
   components: {
     FighterInfoComponent,
     AbilityComponent,
+    ItemComponent,
   },
 
   data () {
@@ -40,6 +56,7 @@ export default {
       fightingMob: null,
       abilities: null,
       winner: null,
+      droppedItem: null,
     };
   },
 
@@ -52,6 +69,35 @@ export default {
       this.$socket.emit('userLeaveFight');
     }
     next();
+  },
+
+  methods: {
+    getData: function () {
+      const locationId = this.$route.params.locationId;
+      const token = this.$store.state.token;
+      this.$socket.emit('getData', { token, locationId });
+    },
+    attack: function () {
+      this.$socket.emit('attack');
+    },
+
+    areFighting: function () {
+      return this.fightingHero && this.fightingMob;
+    },
+    replaceAll: function(target, search, replacement) {
+      return target.replace(new RegExp(search, 'g'), replacement);
+    },
+
+    convertRarityToJson: function(item) {
+      let rarity = this.replaceAll(item.rarity, '\'', '"');
+      rarity = '\'' + rarity + '\'';
+      rarity = rarity.slice(1, rarity.length-1);
+      return JSON.parse(rarity);
+    },
+
+    keepItem: function () {
+      this.$socket.emit('saveItemToHero');
+    },
   },
 
   // socket keys are socket emits names 
@@ -79,22 +125,13 @@ export default {
         this.winner.additionalText = 'You lose.'
       }
     },
+    itemDropped: function ({ item, item_statistics }){
+      this.droppedItem = { item, item_statistics};
+      let rarity = this.convertRarityToJson(item);
+      this.droppedItem.item.rarity = rarity;
+    }
   },
 
-  methods: {
-    getData: function () {
-      const locationId = this.$route.params.locationId;
-      const token = this.$store.state.token;
-      this.$socket.emit('getData', { token, locationId });
-    },
-    attack: function () {
-      this.$socket.emit('attack');
-    },
-
-    areFighting: function () {
-      return this.fightingHero && this.fightingMob;
-    },
-  },
 };
 </script>
 <style lang="scss" scoped>
