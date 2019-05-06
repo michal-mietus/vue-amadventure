49494d2667cff1cddfadd8019e9f59bd19dda574
goodude123 <assasinmcpl@gmail.com>
2019-05-06 16:54:42 +0200

    Single body part component.

---
+++ src/components/hero/owned/BodyPartComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ bodyPart }}: </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+
+  props: {
+    bodyPart: {},
+    item: {},
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
