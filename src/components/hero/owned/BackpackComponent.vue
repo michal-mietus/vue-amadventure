7e925dc507d8c6bb79611c94a9faf031bf645aae
goodude123 <assasinmcpl@gmail.com>
2019-05-06 16:53:39 +0200

    Backpack items rendered component.

---
+++ src/components/hero/owned/BackpackComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>Backpack</p>
+    <div v-for="item in items" :key="item.id">
+      <ItemComponent :item="item"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemComponent from '@/components/artifical/ItemComponent.vue';
+
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+
+  components: {
+    ItemComponent,
+  },
+
+  props: {
+    items: [],
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
