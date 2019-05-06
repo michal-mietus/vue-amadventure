8ddf083d4680e97dd9a2436ef8151047fe4c63ae
goodude123 <assasinmcpl@gmail.com>
2019-05-06 16:54:27 +0200

    Component for items and body parts on which they are put.

---
+++ src/components/hero/owned/PutOnItemsComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>Equipped Items</p>
+    <div v-for="bodyPartWithItem in bodyPartsWithItems" :key="bodyPartWithItem.id">
+      <BodyPartComponent
+        :bodyPart="bodyPartWithItem.name"
+        :item="bodyPartWithItem.item"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BodyPartComponent from '@/components/hero/owned/BodyPartComponent.vue';
+
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  
+  components: {
+    BodyPartComponent,
+  },
+
+  props: {
+    bodyPartsWithItems: [],
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
